21b1a635166ca12648df02a12134755fb233a9fe
Vitor Soares <vitorsoaresf@gmail.com>
2024-04-30 18:19:09 -0300

    refactor: implements imports external libs

--- maxima-tech/src/routes/Router.tsx
+++ maxima-tech/src/routes/Router.tsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "@lib/reactRouterDom";
 
 export const RoutesApp = () => {
   return (
     <Routes>
       <Route path="/" element={<div>Dashboard</div>}></Route>
-      <Route path="/detailsproduct" element={<div>Details Cart</div>}></Route>
+      <Route path="/detailscart" element={<div>Details Cart</div>}></Route>
       <Route path="/cart" element={<div>Cart</div>}></Route>
     </Routes>
   );
