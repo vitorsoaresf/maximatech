00699a4ea40929a4e744d5af300c156b8c2a6cd2
Vitor Soares <vitorsoaresf@gmail.com>
2024-04-30 17:59:00 -0300

    chore: implements absolute paths to components importations

--- maxima-tech/vite.config.ts
+++ maxima-tech/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@assets": "/src/assets",
+      "@styles": "/src/styles",
+      "@components": "/src/components",
+      "@context": "/src/context",
+      "@interface": "/src/interface",
+      "@lib": "/src/lib",
+      "@pages": "/src/pages",
+      "@routes": "/src/routes",
+      "@services": "/src/services",
+      "@utils": "/src/utils",
+    },
+  },
+});
